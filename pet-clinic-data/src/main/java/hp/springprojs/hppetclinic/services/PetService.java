d7bbd19630e2dcbdf54003edd63f7b5f7861b6f2
harsh_300070019 <harsh.prateek@myntra.com>
2021-08-10 22:04:03 +0530

    Refactor interface service. closes #4

--- pet-clinic-data/src/main/java/hp/springprojs/hppetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/hp/springprojs/hppetclinic/services/PetService.java
@@ -2,12 +2,6 @@ package hp.springprojs.hppetclinic.services;
 
 import hp.springprojs.hppetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/hp/springprojs/hppetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/hp/springprojs/hppetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package hp.springprojs.hppetclinic.services;
 
 import hp.springprojs.hppetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet,Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }

5121c383326b3f9d18b10f2eb09184bdf17572ea
harsh_300070019 <harsh.prateek@myntra.com>
2021-08-10 15:48:55 +0530

    Implement Interface services. closes #2

---
+++ pet-clinic-data/src/main/java/hp/springprojs/hppetclinic/services/OwnerService.java
@@ -0,0 +1,17 @@
+package hp.springprojs.hppetclinic.services;
+
+import hp.springprojs.hppetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+
+}
---
+++ pet-clinic-data/src/main/java/hp/springprojs/hppetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package hp.springprojs.hppetclinic.services;
+
+import hp.springprojs.hppetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
